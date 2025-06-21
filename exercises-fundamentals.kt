c154b313e9bba915fabbb9781dd04d7cbade1ba4
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-06-21 16:30:36 +0530

    3.Movie Ticket Prices

--- exercises-fundamentals.kt
+++ exercises-fundamentals.kt
@@ -1,4 +1,4 @@
-// 2- Mobile Notifications
+// 2. Mobile Notifications
 fun main() {
     val morningNotification = 51
     val eveningNotification = 135
@@ -15,4 +15,40 @@ fun printNotificationSummary(numberOfMessages: Int) {
     }else{
         println("Your phone is blowing up! You have 99+ notifications")
     }
+}
+
+fun main() {
+    val child = 5
+    val adult = 28
+    val senior = 87
+    
+    val isMonday = true
+    
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+}
+
+// 3. Movie Ticket Prices
+
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    // Fill in the code.
+    
+    if(age<0){
+        return -1
+    }else if(age<=12){
+        return 15
+
+    }else if (age<=60){
+        if(isMonday){
+            return 25
+        }else{
+            return 30
+        }
+        
+    }else if(age<=100){
+        return 20
+    }else{
+        return -1
+    } 
 }
