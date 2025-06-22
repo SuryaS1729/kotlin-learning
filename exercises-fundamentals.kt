572844e55c646a6e0f66372c8992081aa0df04d1
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-06-23 02:53:19 +0530

    5.Song Catalog

--- exercises-fundamentals.kt
+++ exercises-fundamentals.kt
@@ -107,3 +107,22 @@ fun printFinalTemperature(
     val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
     println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
 }
+
+//5. Song catalog
+fun main(){
+       val brunoSong = Song("We Don't Talk About Bruno", "Encanto Cast", 2022, 1_000_000)
+    brunoSong.printDescription()
+    println(brunoSong.isPopular)
+}
+class Song(val title: String, val artist: String, val yearPublished: Int, val playCount: Int){
+        val isPopular: Boolean
+          get() = playCount >= 1000
+
+        fun printSongDescription(){
+            println("$title, performed by $artist, was released in $yearPublished.")
+        }
+      
+        
+
+}
+
