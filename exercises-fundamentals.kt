f321cde5aaa18b32b7fe1b220ba29fc34f175282
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-06-23 14:48:18 +0530

    7.Foldable Phone

--- exercises-fundamentals.kt
+++ exercises-fundamentals.kt
@@ -157,3 +157,46 @@ class Person(val name: String, val age: Int, val hobby: String?, val referrer: P
         print("\n\n")
     }
 }
+
+// 7. Foldable Phone
+
+open class Phone(var isScreenLightOn: Boolean = false){
+    open fun switchOn() {
+        isScreenLightOn = true
+    }
+    
+    fun switchOff() {
+        isScreenLightOn = false
+    }
+    
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(var isFolded: Boolean = true): Phone() {
+    override fun switchOn() {
+        if (!isFolded) {
+            isScreenLightOn = true
+        }
+    }
+    
+    fun fold() {
+        isFolded = true
+    }
+    
+    fun unfold() {
+        isFolded = false
+    }
+}
+
+fun main() {    
+    val newFoldablePhone = FoldablePhone()
+    
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.unfold()
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+}
