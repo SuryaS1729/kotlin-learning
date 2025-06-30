40702b7c2705cf84bbaabc2a3aa700ce67abe9e2
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-06-30 18:26:19 +0530

    interfaces as an extension to a class

--- more-kotlin.kt
+++ more-kotlin.kt
@@ -104,3 +104,52 @@ fun Quiz.StudentProgress.printProgressBar() {
 fun main() {
     Quiz.printProgressBar()
 }
+
+
+//rewrite extension functions using an interface
+
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty
+)
+
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+
+interface ProgressPrintable {
+    val progressText: String
+        fun printProgressBar()
+
+}
+
+class Quiz: ProgressPrintable {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    
+    
+override val progressText: String
+        get() = "${answered} of ${total} answered"
+ 
+    
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+    
+override fun printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(progressText)
+}
+}
+
+
+
+fun main() {
+    Quiz().printProgressBar()
+}
