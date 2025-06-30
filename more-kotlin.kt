0b4cd14a6aada603ca27f0de30aa87305df8387c
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-06-30 16:49:32 +0530

    singleton and companion objects in kotlin

--- more-kotlin.kt
+++ more-kotlin.kt
@@ -1,16 +1,10 @@
 //making a resusable class with generics
 
-
 class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: String
 )
-
-
-
-
-
 
 fun main() {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
@@ -20,22 +14,55 @@ fun main() {
 
 // ENUMS IN KOTLIN
 
-
-class Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: String
 )
-
 enum Difficulty {
     EASY, MEDIUM, HARD
 }
-
-
-
-
 fun main() {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 }
+
+//singleton objects in kotlin
+
+object StudentProgress {
+    var total: Int = 10
+    var answered: Int = 3
+}
+fun main() {
+    
+    println("${StudentProgress.answered} of ${StudentProgress.total} answered.")
+}
+
+//companion objects in kotlin
+
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty
+)
+
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+   companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+fun main() {
+    println("${Quiz.answered} of ${Quiz.total} answered.")
+}
+
