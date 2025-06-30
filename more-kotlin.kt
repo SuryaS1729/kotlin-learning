87791bd8020a0bcd9cab4f4835c4f30d8466088a
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-06-30 16:25:28 +0530

    enums in kotlin

--- more-kotlin.kt
+++ more-kotlin.kt
@@ -17,3 +17,25 @@ fun main() {
     val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
     val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
 }
+
+// ENUMS IN KOTLIN
+
+
+class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: String
+)
+
+enum Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+
+
+
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+}
