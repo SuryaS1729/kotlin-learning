703117ba30ca0fa6386520ac33f1be630e900aa4
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-07-02 13:49:28 +0530

    forEach, map, filter

---
+++ higher-order-functions.kt
@@ -0,0 +1,78 @@
+//forEach()
+class Cookie(
+    val name: String,
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    val price: Double
+)
+val cookies = listOf(
+    Cookie(
+        name = "Chocolate Chip",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.69
+    ),
+    Cookie(
+        name = "Banana Walnut", 
+        softBaked = true, 
+        hasFilling = false, 
+        price = 1.49
+    ),
+    Cookie(
+        name = "Vanilla Creme",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.59
+    ),
+    Cookie(
+        name = "Chocolate Peanut Butter",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Snickerdoodle",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.39
+    ),
+    Cookie(
+        name = "Blueberry Tart",
+        softBaked = true,
+        hasFilling = true,
+        price = 1.79
+    ),
+    Cookie(
+        name = "Sugar and Sprinkles",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.39
+    )
+)
+//forEach
+fun main() {
+	 cookies.forEach {
+        println("Menu item: ${it.name}")
+    }
+}
+//map
+fun main() {
+	val fullMenu = cookies.map {
+    "${it.name} - $${it.price}"
+}
+    println("Full Menu:")
+    fullMenu.forEach{
+        println(it)
+    }
+}
+//filter()
+fn main(){
+      val softBakedMenu = cookies.filter {
+    it.softBaked
+}
+    println("")
+    println("Soft cookies:")
+softBakedMenu.forEach {
+    println("${it.name} - $${it.price}")
+}
+}
