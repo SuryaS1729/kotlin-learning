48458a261399465df3748bed193a4a30e02faade
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-07-16 23:03:00 +0530

    exceptions, cancellations, async

--- coroutines-in-kotlin.kt
+++ coroutines-in-kotlin.kt
@@ -22,3 +22,158 @@ suspend fun printTemperature() {
     delay(1000)
     println("30\u00b0C")
 } 
+
+// concurrency , faster
+
+fun main() {
+    val time = measureTimeMillis {
+        runBlocking {
+            println("Weather forecast")
+            launch {
+                printForecast()
+            }
+            launch {
+                printTemperature()
+            }
+        }
+    }
+    println("Execution time: ${time / 1000.0} seconds")
+}
+
+suspend fun printForecast() {
+    delay(1000)
+    println("Sunny")
+}
+
+suspend fun printTemperature() {
+    delay(1000)
+    println("30\u00b0C")
+} 
+// its much faster here as the system is not waiting for a particular task to be completed, all
+// pieces of code in the launch {} are pushed to execute and when they all return isn when teh program finishes executing
+//launch is fire and forget, might be useful but it
+//async waits until the response is back before moving to the next line
+fun main() {
+    runBlocking {
+        println("Weather forecast")
+        launch {
+            printForecast()
+        }
+        launch {
+            printTemperature()
+        }
+        println("Have a good day!")
+    }
+}
+
+suspend fun printForecast() {
+    delay(1000)
+    println("Sunny")
+}
+
+suspend fun printTemperature() {
+    delay(1000)
+    println("30\u00b0C")
+} 
+
+//now with async
+fun main() {
+    runBlocking {
+        println("Weather forecast")
+        val forecast: Deferred<String> = async {
+            getForecast()
+        }
+        val temperature: Deferred<String> = async {
+            getTemperature()
+        }
+        println("${forecast.await()} ${temperature.await()}")
+        println("Have a good day!")
+    }
+}
+
+suspend fun getForecast(): String {
+    delay(1000)
+    return "Sunny"
+}
+
+suspend fun getTemperature(): String {
+    delay(1000)
+    return "30\u00b0C"
+}
+// parallel decomposition
+import kotlinx.coroutines.*
+
+fun main() {
+    runBlocking {
+        println("Weather forecast")
+        println(getWeatherReport())
+        println("Have a good day!")
+    }
+}
+
+suspend fun getWeatherReport() = coroutineScope {
+    val forecast = async { getForecast() }
+    val temperature = async { getTemperature() }
+    "${forecast.await()} ${temperature.await()}"
+}
+
+suspend fun getForecast(): String {
+    delay(1000)
+    return "Sunny"
+}
+
+suspend fun getTemperature(): String {
+    delay(1000)
+    return "30\u00b0C"
+}
+// error handling
+import kotlinx.coroutines.*
+
+fun main() {
+    runBlocking {
+        println("Weather forecast")
+        println(getWeatherReport())
+        println("Have a good day!")
+    }
+}
+
+suspend fun getWeatherReport() = coroutineScope {
+    val forecast = async { getForecast() }
+    val temperature = async {
+        try {
+            getTemperature()
+        } catch (e: AssertionError) {
+            println("Caught exception $e")
+            "{ No temperature found }"
+        }
+    }
+
+    "${forecast.await()} ${temperature.await()}"
+}
+
+suspend fun getForecast(): String {
+    delay(1000)
+    return "Sunny"
+}
+
+suspend fun getTemperature(): String {
+    delay(500)
+    throw AssertionError("Temperature is invalid")
+    return "30\u00b0C"
+}
+
+//cancelling a coroutine
+
+...
+
+suspend fun getWeatherReport() = coroutineScope {
+    val forecast = async { getForecast() }
+    val temperature = async { getTemperature() }
+    
+    delay(200)
+    temperature.cancel()
+
+    "${forecast.await()}"
+}
+
+...
